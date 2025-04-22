2df8f5b4ff0f63756ca31f49988a93dc497587bc
JonHuang1 <jonhuang918@gmail.com>
2025-04-22 16:08:01 -0400

    rust book io project minigrep completed up to ch 12.4

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,53 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+/*
+ * "new" changed to "build" because programmers never expect "new" to fail 
+ */
+//    fn new(args: &[String]) -> Config {
+//        if args.len() < 3 {
+//            panic!("not enough arguments");
+//        }
+
+//        let query: String = args[1].clone();
+//        let file_path: String = args[2].clone();
+
+//        Config { query, file_path }
+//    }
+
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+/*
+ * logic moved into Config constructor
+ */
+//fn _parse_config(args: &[String]) -> Config {
+//    let query: String = args[1].clone();
+//    let file_path: String = args[2].clone();
+
+//    Config { query, file_path }
+//} 
+
---
+++ minigrep/src/main.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+//    let query = &args[1];
+//    let file_path = &args[2];
+//    let config: Config = parse_config(&args);
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argumetns: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for \"{}\"", config.query);
+    println!("In file \"{}\"", config.file_path);
+//    let contents = fs::read_to_string(config.file_path)
+//        .expect("Should have been able to read the file");
+
+//    println!("With text:\n{contents}");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }   
+}
+
